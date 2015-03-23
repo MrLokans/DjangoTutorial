b17b74dcc210d0e458bbc86ef8ef32cbf179cec6
MrLokans <trikster1911@gmail.com>
2015-03-23 07:35:18 +0300

    Improved.

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -1,7 +1,27 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
 # from django.template import RequestContext, loader
-from polls.models import Question
+from polls.models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    contex_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
@@ -20,5 +40,20 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {"question": question})
 
 
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {"question": question})
+
+
 def vote(request, question_id):
-    return HttpResponse("Yo're voting for {}".format(question_id))
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.hmll',
+            {'question': p, "error_message": "No choice made."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
